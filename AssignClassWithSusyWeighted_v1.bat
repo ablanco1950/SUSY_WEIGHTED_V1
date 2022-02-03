b6c1f5f5dbc9eebcc1d7865cbf655b59bec9be3a
Alfonso Blanco <alfonsoblanco5050@gmail.com>
2022-02-03 17:46:46 +0100

    Add files via upload

--- AssignClassWithSusyWeighted_v1.bat
+++ AssignClassWithSusyWeighted_v1.bat
@@ -1,1 +1,1 @@
-java -jar AssignClassWithSusyWeighted_v1.jar c:\SusyWeighted78PercentHits.txt c:\SUSY.csv 0.0  4500000.0    4500000.0  5000000.0
+java -jar AssignClassWithSusyWeighted_v1.jar C:\SUSY_WEIGHTED_V1\SusyWeighted78PercentHits.txt c:\SUSY.csv 0.0  4500000.0  4500000.0  5000000.0  C:\SUSY_WEIGHTED_V1\FileTextWithClassAssigned.txt
